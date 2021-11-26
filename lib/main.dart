e15a56bf306afd236d94b667ef95eca52b79832c
KYEONGMIN CHO <ggingmin@gmail.com>
2021-11-27 00:46:33 +0900

    image added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("GET SET!"),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text(
+              "ggingmin",
+              style: GoogleFonts.pressStart2p(),
+          ),
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.black,
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://www.ggingmin.com/_next/image?url=%2Fimages%2Fggingmin_mimoji.png&w=256&q=75'),
+          ),
+        ),
       ),
     ),
   );
